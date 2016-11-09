b4c84999f84e1f1712591c39082bfee3cd73e61c
tianNanYiHao <851085835@qq.com>
2016-11-09 10:14:01 +0800

    123

--- DSWeiBo/DSWeiBo/classes/Main/MainViewController.swift
+++ DSWeiBo/DSWeiBo/classes/Main/MainViewController.swift
@@ -14,27 +14,56 @@ class MainViewController: UITabBarController {
         super.viewDidLoad()
         //设置当前控制器的颜色为原色
         tabBar.tintColor = UIColor.orange
-        addChildViewController(HomeTableViewController(), "首页", "tabbar_home", "")
-        addChildViewController(MessageTableViewController(), "消息", "tabbar_message_center", "")
-        addChildViewController(DiscoverTableViewController(), "发现", "tabbar_discover", "")
-        addChildViewController(ProfileTableViewController(), "我", "tabbar_profile", "tabbar_profile")
+        
+        
+        //1获取字典
+        let path =  Bundle.main.path(forResource: "MainVCSettings.json", ofType: nil)
+        //动态绑定
+        if let jsonPath = path {
+            //2 通过路径创建NSData
+            let data = NSData.init(contentsOfFile: jsonPath)
+            
+            //3.序列化json数据,-->Array
+            // do try 操作
+            do {
+                let dictArr =  try JSONSerialization.jsonObject(with: data! as Data, options: JSONSerialization.ReadingOptions.mutableContainers)
+                print(dictArr)
+                for dict in dictArr as! [[String:String]]{
+                    // 取字符(可从后台获取) 创建类
+                    addChildViewController(dict["vcName"]!, dict["title"]!, dict["imageName"]!)
+                }
+            } catch {
+                //处理异常
+                print(error)
+            }
+        }
+        
         
     }
 
     
-    
-    
     //MARK: - 私有的封装方法 : 创建导航设置图片以及标题
-    private func addChildViewController(_ childController: UIViewController , _ titleName:String , _ imageDefaule:String , _ imageSelect:String) {
-        childController.tabBarItem.image = UIImage.init(imageLiteralResourceName: imageDefaule)
-        childController.tabBarItem.selectedImage = UIImage.init(imageLiteralResourceName: imageDefaule+"_highlighted")
-        let nav = UINavigationController.init(rootViewController: childController)
-        childController.tabBarItem.title = titleName
-        childController.navigationItem.title = titleName
+    private func addChildViewController(_ childControllerName: String , _ titleName:String , _ imageDefaule:String) {
+        //动态获取命名空间 (从plist中获取) CFBundleExecutable
+        let productName =  Bundle.main.infoDictionary!["CFBundleExecutable"] as! String
+        //1 根据字符串创建类
+        let className:AnyClass? = NSClassFromString(productName+"."+childControllerName)
+        print(className)
+        //2 绑定类的类型为 UIViewController  (将AnyClass 转换位 指定的类型)
+        let classCtr = className as! UIViewController.Type
+        //3 根据自定义的类 来创建对象
+        let vc = classCtr.init()
+        
+        //设置vc
+        vc.tabBarItem.image = UIImage.init(imageLiteralResourceName: imageDefaule)
+        vc.tabBarItem.selectedImage = UIImage.init(imageLiteralResourceName: imageDefaule+"_highlighted")
+        let nav = UINavigationController.init(rootViewController: vc)
+        vc.tabBarItem.title = titleName
+        vc.navigationItem.title = titleName
         addChildViewController(nav)
     }
     
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
